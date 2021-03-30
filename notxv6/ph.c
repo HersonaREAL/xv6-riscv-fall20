bf13af4735efadeef995a79b47db979ccf40eb3b
010101010-YCJ <woxi531442790@gmail.com>
2021-03-30 23:03:50 +0800

    past ph safe

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock;
 
 double
 now()
@@ -46,13 +47,17 @@ void put(int key, int value)
     if (e->key == key)
       break;
   }
+  
   if(e){
     // update the existing key.
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);
   }
+  
 }
 
 static struct entry*
@@ -102,6 +107,7 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+  pthread_mutex_init(&lock, NULL);
 
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
