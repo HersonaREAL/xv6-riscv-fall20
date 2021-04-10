e7e37b45b1ebde352bc1df070e32a0f63882d82d
010101010-YCJ <woxi531442790@gmail.com>
2021-04-10 14:55:10 +0800

    kalloc complete

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -7,6 +7,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "riscv.h"
+#include "proc.h"
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
@@ -18,15 +19,32 @@ struct run {
   struct run *next;
 };
 
+// struct {
+//   struct spinlock lock;
+//   struct run *freelist;
+// } kmem;
+
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} ckmem[NCPU];
+
+int getcpuid() {
+  push_off();
+  int res = cpuid();
+  pop_off();
+  return res;
+}
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  //for lock of each cpu list
+  for (int i = 0; i < NCPU; ++i) {
+    initlock(&ckmem[i].lock, "kmem");
+  }
+  
+  //initlock(&kmem.lock, "kmem");
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -35,7 +53,7 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
     kfree(p);
 }
 
@@ -47,6 +65,7 @@ void
 kfree(void *pa)
 {
   struct run *r;
+  int cpuid;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
@@ -56,10 +75,18 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  //get cpu id
+  cpuid = getcpuid();
+
+
+  //operate cpu free list
+  acquire(&ckmem[cpuid].lock);
+
+  r->next = ckmem[cpuid].freelist;
+  ckmem[cpuid].freelist = r;
+
+  release(&ckmem[cpuid].lock);
+
 }
 
 // Allocate one 4096-byte page of physical memory.
@@ -69,12 +96,43 @@ void *
 kalloc(void)
 {
   struct run *r;
+  int cpuid;
+  //get cpu id
+  cpuid = getcpuid();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+
+
+  acquire(&ckmem[cpuid].lock);
+  r = ckmem[cpuid].freelist;
+  //has page?
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    ckmem[cpuid].freelist = r->next;
+  else {
+
+    //steal mem from other cpus
+    for (int i = 0; i < NCPU;++i) {
+
+      //check left page
+      if(ckmem[i].freelist) {
+        acquire(&ckmem[i].lock);
+
+        //check again
+        if(ckmem[i].freelist) {
+          
+          //just steal one page
+          r = ckmem[i].freelist;
+          ckmem[i].freelist = r->next;
+
+          release(&ckmem[i].lock);
+          break;
+        }
+        else
+          release(&ckmem[i].lock);
+      }
+    }
+  }
+  release(&ckmem[cpuid].lock);
+
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
